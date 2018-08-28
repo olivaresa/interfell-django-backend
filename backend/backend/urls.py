911a274b03aead987b4b5987c63b924a33d1bd9e
Alexander Olivares <olivaresa@gmail.com>
2018-08-27 21:01:31 -0400

    set users/ endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,9 +18,18 @@ from django.urls import path, re_path
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
+from rest_framework import routers
+from users import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api-token-auth/', obtain_jwt_token),
     re_path(r'^api-token-refresh/', refresh_jwt_token),
     re_path(r'^api-token-verify/', verify_jwt_token),
 ]
+
+urlpatterns += router.urls
